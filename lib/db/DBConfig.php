5d44393623494f6a5085c9578b2d2b7a97b0e1c2
root <root@i-59qq9m73.(none)>
2017-12-23 16:10:18 +0800

    init

---
+++ lib/db/DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * DB配置
+ */
+ class DBConfig
+{
+    const HOST = 'localhost';
+    const PORT = '3306';
+    const USER = 'root';
+    const PASSWORD = 'Root@123';
+    // DB NAME
+    const DATABASE = 'sxb_db';
+    const CHARSET = 'utf8mb4';
+}
+?>
