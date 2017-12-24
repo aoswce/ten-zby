9142a718e1f8ae342709a31c866425b27a7ea614
wvv <aoswce@126.com>
2017-12-24 20:37:30 +0800

    user

--- service/account/AccountRegisterCmd.php
+++ service/account/AccountRegisterCmd.php
@@ -104,7 +104,7 @@ class AccountRegisterCmd extends Cmd
         //var_dump($api->usersig);
         $ret = $api->account_import($uid,$nick,$face_url);
         //echo "==============User-Ret:=================================";
-        var_dump($ret);
+        //var_dump($ret);
         return $ret;
     }
 }
