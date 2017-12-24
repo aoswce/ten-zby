52f93cd8f3fbd3c82bea454d402068710d5de5d8
wvv <aoswce@126.com>
2017-12-24 10:37:58 +0800

    date

--- service/live/CreateLiveRoomCmd.php
+++ service/live/CreateLiveRoomCmd.php
@@ -108,7 +108,7 @@ class CreateLiveRoomCmd extends TokenCmd
         //if(intval($ret['errCode']) > 0){
         //    return new CmdResp(ERR_LIVE_NO_AV_ROOM_ID,'Server error :report root info fail!');
         //}
-        $expire = strtotime("Y-m-d H:i:s",date("+1 day"));
+        $expire = date('Y-m-d H:i:s',strtotime("+1 day"));
         $streamId = $id.substr(md5($id),0,9);
         $pushUrl = $this->getPushUrl(DEFAULT_SDK_APP_BIZ,$streamId,APP_SECURITY_KEY,$expire);
         return new CmdResp(
